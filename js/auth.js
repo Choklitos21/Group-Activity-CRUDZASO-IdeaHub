371074ecd92cc3980e5309a8ed92b90e678e9091
estiven-piedrahita <duvanestivenpiedrahita@gmail.com>
2026-01-27 15:53:56 -0500

    termino login y register pero faltan detalles

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,21 @@
-
-
-
 let users = JSON.parse(localStorage.getItem("users")) || [];
 let message = document.querySelector(".message");
+let form = document.getElementById("registerForm");
+let theForm = document.querySelector(".the-form");
+
+// eventos
+if(form){form.addEventListener("submit", register)};  
+if(theForm){theForm.addEventListener("submit", login)}
 
 // funcion para register
-function register(name,email,password) {
+function register(e) {
+    e.preventDefault()
+    let name = document.getElementById("the-name").value.trim();
+    let email = document.getElementById("the-email").value.trim();
+    let password = document.getElementById("the-password").value.trim();
+
 
     let exists = users.some(user=>user.email === email);
-    
     
     if (exists) {
         message.textContent = "There is already an account with that email";
@@ -21,23 +28,40 @@ function register(name,email,password) {
     users.push({name,email,password});
     localStorage.setItem("users", JSON.stringify(users));
 
+
+
     message.textContent = "account created successfully";
         message.style.color = "green";
         setTimeout(()=>{ message.textContent = ""},3000)
         form.reset();
 
+}
+function login(e) {
+    e.preventDefault();
+    let emailInput = document.querySelector(".email123").value.trim();
+    let passwordInput = document.querySelector(".password123").value.trim();
+
+
+    let userFound = users.find(user=> user.email === emailInput && user.password === passwordInput )
+
+    if (!userFound) {
+        message.textContent = "invalid data";
+        message.style.color = "red";
+        setTimeout(()=>{message.textContent = ""},2000)
+        theForm.reset();
+        return;
+    }
+
+    message.textContent = "successful login";
+    message.style.color = "green";
+    setTimeout(()=>{message.textContent = "";
+        window.location.href = "#"},2000);
+        theForm.reset();
+
+    sessionStorage.setItem("loggedUser",emailInput);
 
 }
-let form = document.getElementById("registerForm");
-// evento para registrar
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-
-    let name = document.getElementById("the-name").value.trim();
-    let email = document.getElementById("the-email").value.trim();
-    let password = document.getElementById("the-password").value.trim();
-
-    register(name,email,password);
 
 
-})
+
+
